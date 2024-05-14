a66c8f4992640625ebd1c8afaacb9b0a1c81ad98
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-05-13 22:00:23 -0300

    feat: renderiza filmes e hqs

--- src/pages/Filmes/index.tsx
+++ src/pages/Filmes/index.tsx
@@ -1,11 +1,23 @@
+import { Card } from "../../components/Card";
 import { Header } from "../../components/Header";
+import { FilmesData } from "../../data/filmesData";
 
 const Filmes = () => {
     return (
         <>
             <Header />
             <main>
-                <h1>Filmes Page</h1>
+                <section>
+                    {FilmesData.map((filme, index) => (
+                        <Card
+                            key={index}
+                            id={filme.id}
+                            imagem={filme.imagem}
+                            nome={filme.nome}
+                            resumo={filme.resumo}
+                        />
+                    ))}
+                </section>
             </main>
         </>
     );
