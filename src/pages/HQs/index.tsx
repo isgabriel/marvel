d84ec66dd78bd3bdc634e77a5b8731bdaf85f4bc
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-05-13 22:00:30 -0300

    feat: renderiza filmes e hqs

--- src/pages/HQs/index.tsx
+++ src/pages/HQs/index.tsx
@@ -1,11 +1,23 @@
+import { Card } from "../../components/Card";
 import { Header } from "../../components/Header";
+import { HqsData } from "../../data/hqsData";
 
 const HQs = () => {
     return (
         <>
             <Header />
             <main>
-                <h1>HQs Page</h1>
+                <section>
+                    {HqsData.map((hq, index) => (
+                        <Card
+                            key={index}
+                            id={hq.id}
+                            imagem={hq.imagem}
+                            nome={hq.nome}
+                            resumo={hq.resumo}
+                        />
+                    ))}
+                </section>
             </main>
         </>
     );
