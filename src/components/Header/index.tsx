5a53da75b8c54187df37f298085dad1e1f792a23
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-05-15 00:46:28 -0300

    feat: cria header responsivo, estilos basicos para logica funcionar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,130 @@
-import { Link } from "react-router-dom";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { Link, useLocation } from "react-router-dom";
 import { useUser } from "../../hooks/useUser";
+import {
+    HeaderContainer,
+    Logo,
+    MenuIcon,
+    NavLink,
+    NavbarContainer,
+    NavbarContainerDesktop,
+} from "./styles";
 
 const Header = () => {
+    const [menuAberto, setMenuAberto] = useState(false);
+    const [selecionado, setSelecionado] = useState("");
+
     const { logout } = useUser();
+
+    const location = useLocation();
+
+    const cliqueMenuHamburguer = () => {
+        setMenuAberto(!menuAberto);
+    };
+
+    const cliqueNavItem = (item: string) => {
+        setSelecionado(item);
+
+        setMenuAberto(false);
+    };
+
     return (
-        <header>
-            <p>Header</p>
-            <nav>
-                <div>
-                    <Link to="/personagens">Perosnagens</Link>
-                    <Link to="/filmes">Filmes</Link>
-                    <Link to="/hqs">HQs</Link>
+        <>
+            <HeaderContainer>
+                <div className="firstLineContainer">
+                    <Link to="/personagens">
+                        <Logo src="/assets/logo/logo.svg" alt="Logo" />
+                    </Link>
+                    <MenuIcon onClick={cliqueMenuHamburguer}>
+                        {menuAberto ? <FiX /> : <FiMenu />}
+                    </MenuIcon>
                 </div>
-                <button onClick={logout}>Sair</button>
-            </nav>
-        </header>
+                {menuAberto && (
+                    <NavbarContainer>
+                        <div>
+                            <img
+                                src="/assets/icones/usuario.svg"
+                                alt="foto do usuário"
+                            />
+                            <button onClick={logout}>Sair</button>
+                        </div>
+                        <NavLink
+                            to="/personagens"
+                            className={
+                                location.pathname === "/personagens"
+                                    ? "selecionado"
+                                    : ""
+                            }
+                            onClick={() => cliqueNavItem("personagens")}
+                        >
+                            Personagens
+                        </NavLink>
+                        <NavLink
+                            to="/filmes"
+                            className={
+                                location.pathname === "/filmes"
+                                    ? "selecionado"
+                                    : ""
+                            }
+                            onClick={() => cliqueNavItem("filmes")}
+                        >
+                            Filmes
+                        </NavLink>
+                        <NavLink
+                            to="/hqs"
+                            className={
+                                location.pathname === "/hqs"
+                                    ? "selecionado"
+                                    : ""
+                            }
+                            onClick={() => cliqueNavItem("hqs")}
+                        >
+                            HQs
+                        </NavLink>
+                    </NavbarContainer>
+                )}
+                <NavbarContainerDesktop>
+                    <NavLink
+                        to="/personagens"
+                        className={
+                            location.pathname === "/personagens"
+                                ? "selecionado"
+                                : ""
+                        }
+                        onClick={() => cliqueNavItem("personagens")}
+                    >
+                        Personagens
+                    </NavLink>
+                    <NavLink
+                        to="/filmes"
+                        className={
+                            location.pathname === "/filmes" ? "selecionado" : ""
+                        }
+                        onClick={() => cliqueNavItem("filmes")}
+                    >
+                        Filmes
+                    </NavLink>
+                    <NavLink
+                        to="/hqs"
+                        className={
+                            location.pathname === "/hqs" ? "selecionado" : ""
+                        }
+                        onClick={() => cliqueNavItem("hqs")}
+                    >
+                        HQs
+                    </NavLink>
+                    <div>
+                        <img
+                            src="/assets/icones/usuario.svg"
+                            alt="foto do usuário"
+                        />
+                        <button onClick={logout}>Sair</button>
+                    </div>
+                </NavbarContainerDesktop>
+            </HeaderContainer>
+        </>
     );
 };
 
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,85 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    display: flex;
+    flex-direction: column;
+
+    background-color: #6f1315;
+
+    .firstLineContainer {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        padding: 10px 20px;
+    }
+    @media (min-width: 769px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+`;
+
+export const Logo = styled.img`
+    width: 35%;
+    max-width: 400px;
+
+    height: auto;
+`;
+
+export const MenuIcon = styled.div`
+    font-size: 24px;
+
+    color: #fff;
+
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+
+    @media (min-width: 769px) {
+        display: none;
+    }
+
+    &:hover {
+        color: #ccc;
+    }
+`;
+
+export const NavbarContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    gap: 7px;
+
+    padding: 10px 20px;
+
+    background-color: #6f1315;
+
+    @media (min-width: 769px) {
+        display: none;
+        flex-direction: row;
+        align-items: center;
+    }
+`;
+export const NavbarContainerDesktop = styled.nav`
+    display: none;
+
+    padding: 10px 20px;
+    background-color: #6f1315;
+
+    @media (min-width: 769px) {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+`;
+
+export const NavLink = styled(Link)`
+    color: #84848d;
+    text-decoration: none;
+
+    &.selecionado {
+        color: #fff;
+    }
+`;
